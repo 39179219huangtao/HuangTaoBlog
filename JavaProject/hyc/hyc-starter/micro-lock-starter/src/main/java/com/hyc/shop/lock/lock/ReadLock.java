d4f5df6d9509712bbed3143c5e744eba690efc44
黄涛 <huangtao@icsoc.net>
2019-06-12 14:44:17 +0800

    first

--- JavaProject/hyc/hyc-starter/micro-lock-starter/src/main/java/com/hyc/shop/lock/lock/ReadLock.java
+++ JavaProject/hyc/hyc-starter/micro-lock-starter/src/main/java/com/hyc/shop/lock/lock/ReadLock.java
@@ -9,6 +9,8 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * @description:
+ * 当写操作时，其他分布式进程/线程无法读取或写入数据；当读操作时，其他分布式进程/线程无法写入数据，但可以读取数据
+ * 允许同时有多个读锁，但是最多只能有一个写锁。多个读锁不互斥，读锁与写锁互斥
  * @author: huangtao
  * @date: 2019/6/12
  */
