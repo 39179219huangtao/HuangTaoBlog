2c2f25820a7df2cd0237d8b2f836d2b04c5739eb
黄涛 <huangtao@icsoc.net>
2019-07-02 16:27:54 +0800

    增加swagger

---
+++ JavaProject/hyc/hyc-web/src/main/java/com/hyc/shop/web/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.hyc.shop.web.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.hyc.shop.admin.service.IHelloService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program hyc
+ * @description:
+ * @author: huangtao
+ * @create: 2019/07/02 16:16
+ */
+
+@RestController
+public class TestController {
+    @Reference
+    private IHelloService helloService;
+
+    @GetMapping("/test")
+    @ApiOperation(value = "测试Grpc调用", notes = "测试Grpc调用")
+    public String test() {
+        helloService.hello();
+
+        return "";
+    }
+
+}
