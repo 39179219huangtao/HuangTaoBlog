139fc68e9b42e40894a961acd8ad88380deaf295
黄涛 <huangtao@icsoc.net>
2019-07-02 16:32:32 +0800

    增加swagger

--- JavaProject/hyc/hyc-web/src/main/java/com/hyc/shop/WebApplicationMain.java
+++ JavaProject/hyc/hyc-web/src/main/java/com/hyc/shop/WebApplicationMain.java
@@ -3,7 +3,11 @@ package com.hyc.shop;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @description:
+ * @author: huangtao
+ * @date: 2019/7/2
+ */
 @SpringBootApplication
 @EnableDubboConfiguration
 public class WebApplicationMain {
