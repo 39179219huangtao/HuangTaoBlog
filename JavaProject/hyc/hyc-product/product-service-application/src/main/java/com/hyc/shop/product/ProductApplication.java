9319d9e8e1f68db2f97f592719adffb00c6050c3
黄涛 <huangtao@icsoc.net>
2019-07-05 14:55:52 +0800

    product服务

--- JavaProject/hyc/hyc-product/product-service-application/src/main/java/com/hyc/shop/product/ProductApplication.java
+++ JavaProject/hyc/hyc-product/product-service-application/src/main/java/com/hyc/shop/product/ProductApplication.java
@@ -1,11 +1,13 @@
 package com.hyc.shop.product;
 
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = {"com.hyc.shop.product"})
 @EnableAsync(proxyTargetClass = true)
+@EnableDubboConfiguration
 public class ProductApplication {
 
 	public static void main(String[] args) {
